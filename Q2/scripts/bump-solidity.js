5fb71153e0b53af506cc359bc1acb62035efb6d7
L-tech <legendabrahamonoja@gmail.com>
2022-05-09 11:53:16 +0100

    introduct to zkp assignment

--- Q2/scripts/bump-solidity.js
+++ Q2/scripts/bump-solidity.js
@@ -10,3 +10,21 @@ bumped = bumped.replace(verifierRegex, 'contract HelloWorldVerifier');
 fs.writeFileSync("./contracts/HelloWorldVerifier.sol", bumped);
 
 // [assignment] add your own scripts below to modify the other verifier contracts you will build during the assignment
+//groth-16 verifier
+content = fs.readFileSync('./contracts/Multiplier3-groth16.sol', {
+    encoding: 'utf-8',
+})
+bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0')
+bumped = bumped.replace(verifierRegex, 'contract MultiplierGrothVerifier')
+
+fs.writeFileSync('./contracts/Multiplier3-groth16.sol', bumped)
+  
+//plonk verifier
+content = fs.readFileSync('./contracts/Multiplier3-plonk.sol', {
+    encoding: 'utf-8',
+})
+
+bumped = content.replace(solidityRegex, 'pragma solidity ^0.8.0')
+bumped = bumped.replace(verifierRegex, 'contract MultiplierPlonkVerifier')
+
+fs.writeFileSync('./contracts/Multiplier3-plonk.sol', bumped)
